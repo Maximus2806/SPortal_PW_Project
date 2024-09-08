6444f52dd642f55d73b04616685c8dd1db4a451c
Maximus2806 <marozaumaksim@gmail.com>
2024-09-08 15:10:32 +0200

    Changed url for global-teardown

--- src/config/global-teardown.ts
+++ src/config/global-teardown.ts
@@ -1,9 +1,9 @@
-import notificationService from "../services/notification.service";
+import notificationService from '../services/notification.service';
 
 export default async function () {
-  if (process.env.ENVIRONMENT === "ci") {
+  if (process.env.ENVIRONMENT === 'ci') {
     await notificationService.postNotification(
-      `Test run funished:\n link to report: https://anatoly-karpovich.github.io/aqa-pw-2/allure-report/#`
+      `Test run funished:\n link to report: https://maximus2806.github.io/SPortal_PW_Project/allure-report/#`
     );
   }
 }
