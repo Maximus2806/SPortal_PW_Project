d46ca6c784629a3fc7ddfb22c9dd1c3558e1b205
danikkrapivnitskit <danikkrapivnitskiy02007@gmail.com>
2024-09-14 12:04:10 +0200

    prepare create order page

--- src/ui/pages/salesPortal.page.ts
+++ src/ui/pages/salesPortal.page.ts
@@ -11,4 +11,10 @@ export abstract class SalesPortalPage extends BasePage {
   async waitForSpinnerToHide() {
     await this.waitForElement(this.spinner, 'hidden');
   }
+
+  async getHeaderPosition(header: string) {
+    const elements = await this.findElements(`//thead//th`);
+    const elementsArray = await Promise.all(elements.map(async (el) => await this.getText(el)));
+    return elementsArray.length ? elementsArray.indexOf(header) + 1 : -1;
+  }
 }
